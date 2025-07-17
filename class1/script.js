8f69dca785cc32aed0a46ec273faecbd550ab507
3suji3 <tnwl2799@gmail.com>
2025-07-17 12:19:23 +0900

    feat: JS Project 1-2~1-3 추가사항 - 자바스크립트 프로젝트1-2~1-3 코드 부분을 추가하여 커밋하였다.

--- class1/script.js
+++ class1/script.js
@@ -30,7 +30,81 @@ const updateHelperText = (input, message, isValid) => {
     helperText.innerText = message;
   }
 
+};
+
+const checkEmptyInput = (input) => {
+  if(input.value.trim() === ""){
+    //인풋입력칸에 입력한 문자열중 띄어쓰기를 없애는 기능
+    updateHelperText(input, '값을 입력해주세요.', false);
+    return false;
+  } else{
+    //입력이 있으면 도움말을 지움
+    updateHelperText(input,"",true);
+    return true;
+  }
 }
+
+//이메일형식이 올바른지 확인하는 함수
+//이메일 주소가 규칙에 맞게 작성되었는지 확인하는 것
+const validateEmailFormat = (input) => {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+  if(emailPattern.test(input.value.trim()) == true){
+    updateHelperText(input,"",true);
+    return true
+  } else{
+    updateHelperText(input,"유효한 이메일 주소를 입력부탁드립니다.",false);
+    return false;
+  }
+}
+
+const checkPasswordStrength = (password) => {
+  const stringPattern = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/
+  if(stringPattern.test(password.value) == true) {
+    updateHelperText(password,"비밀번호 강도: 강함", true);
+    return true;
+  } else{
+    updateHelperText(password, "비밀번호는 8자 이상이어야하며 대문자, 소문자, 특수문자를 포함해야합니다.");
+    return false;
+  }
+}
+
+const validatePasswordMatch = (passwordInput, confirmInput) => {
+  if(passwordInput.value !== confirmInput.value){
+    updateHelperText(confirmInput,"비밀번호가 일치하지 않습니다.",false);
+    return false;
+  } else{
+    updateHelperText(confirmInput,"",true);
+    return true;
+  }
+}
+
+const validatePhoneNumber = (input) => {
+  const phonePattern = /^(01[016789]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
+  if(phonePattern.test(input.value.trim())){
+    updateHelperText(input,"",true);
+    return true;
+  }else{
+    updateHelperText(input, "유효한 전화번호를 입력해주세요.(예:010-1234-5678)");
+    return false;
+  }
+}
+
+const validateForm = () => {
+  //boolean값으로 에러검사시 문제가 없으면 true를 값으로 가지고 있으면 false를 값으로 가진다.
+  const isNicknameValid = checkEmptyInput(userNickname);
+  const isEmailValid = validateEmailFormat(userEmail);
+  const isPasswordStrong = checkPasswordStrength(userPassword);
+  const isPasswordMatch = validatePasswordMatch(userPassword, confirmPassword);
+  const isPhoneValid = validatePhoneNumber(userPhone);
+
+  //모든 검사를 해서 모든 검사가 통과해야 회원가입버튼을 눌렀을 떄 회원가입이 진행되게끔
+  return isNicknameValid&&isEmailValid&&isPasswordMatch&&isPasswordStrong&&isPhoneValid;
+}
+
+registrationForm.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  console.log(e);
+})
 
 
 
